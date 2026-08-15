65f70c3af4df7132d7dabb81d183437339768ccf
Joseph P Medley <medleyj64119@gmail.com>
2026-08-15 10:10:38 -0700

    Improve the build system

--- build.js
+++ build.js
@@ -6,22 +6,25 @@ import manifestJson from './manifest.json' with { type: 'json' };
 function getLocalizedMessages(buildType) {
     const messages = JSON.parse(fs.readFileSync('_locales/en/messages.json', 'utf8'));
     if (buildType === 'test' || buildType === 'beta') {
-        messages.extensionName.message += ` (${buildType.toUpperCase()})`;
+        messages.extensionName.message += ` (${buildType.toUpperCase()}) ${Date.now()}`;
     }
     return messages;
 }
 
 function buildExtension(buildType = 'test') {
     console.log(`Building extension for ${buildType}...`);
+    let filename;
     let output;
     switch (buildType) {
         case 'prod':
-            output = fs.createWriteStream(`${process.env.npm_package_config_builddir}/kramden-${manifestJson.version}.zip`);
+            filename = `${process.env.npm_package_config_builddir}/kramden-${manifestJson.version}.zip`
+            output = fs.createWriteStream(filename);
             break;
         case 'beta':
         case 'test':
         default:
-            output = fs.createWriteStream(`${process.env.npm_package_config_builddir}/kramden-${buildType.toUpperCase()}-${manifestJson.version}.zip`);
+            filename = `${process.env.npm_package_config_builddir}/kramden-${buildType.toUpperCase()}.zip`
+            output = fs.createWriteStream(filename);
             break;
     }
 
@@ -32,22 +35,19 @@ function buildExtension(buildType = 'test') {
     archive.on('warning', (err) => { throw err; });
     archive.pipe(output);
 
-    if (buildType === 'test') {
-        const key = fs.readFileSync('key', 'utf8').trim();
-        const manifestWithKey = {};
-        for (const [name, value] of Object.entries(manifestJson)) {
-            manifestWithKey[name] = value;
-            if (name === 'version') {
-                manifestWithKey.key = key;
+    switch (buildType) {
+        case 'test':
+        case 'beta': {
+            const manifest = structuredClone(manifestJson);
+            manifest.oauth2.client_id = '652869451301-2bu30an73jm424a08ct7jr783eqhkao6.apps.googleusercontent.com';
+            if (buildType === 'test') {
+                manifest.key = fs.readFileSync('key', 'utf8').trim();
             }
+            archive.append(JSON.stringify(manifest, null, 4), { name: 'manifest.json' });
+            break;
         }
-        archive.append(JSON.stringify(manifestWithKey, null, 4), { name: 'manifest.json' });
-    } else if (buildType === 'beta') {
-        const manifestWithClientId = structuredClone(manifestJson);
-        manifestWithClientId.oauth2.client_id = '652869451301-2bu30an73jm424a08ct7jr783eqhkao6.apps.googleusercontent.com';
-        archive.append(JSON.stringify(manifestWithClientId, null, 4), { name: 'manifest.json' });
-    } else {
-        archive.file('manifest.json', { name: 'manifest.json' });
+        default:
+            archive.file('manifest.json', { name: 'manifest.json' });
     }
     archive.file('emailparsers/index.js', { name: 'emailparsers/index.js' });
     archive.file('emailparsers/baseemailparser.js', { name: 'emailparsers/baseemailparser.js' });
@@ -61,6 +61,7 @@ function buildExtension(buildType = 'test') {
     archive.file('emailparsers/linkedinemailparser.js', { name: 'emailparsers/linkedinemailparser.js' });
     archive.file('emailparsers/microsoftemailparser.js', { name: 'emailparsers/microsoftemailparser.js' });
     archive.file('emailparsers/monsteremailparser.js', { name: 'emailparsers/monsteremailparser.js' });
+    archive.file('extensionutils/auth.js', { name: 'extensionutils/auth.js' });
     archive.file('extensionutils/data.js', { name: 'extensionutils/data.js' });
     archive.file('extensionutils/utils.js', { name: 'extensionutils/utils.js' });
     archive.file('extensionutils/emailclient.js', { name: 'extensionutils/emailclient.js' });
